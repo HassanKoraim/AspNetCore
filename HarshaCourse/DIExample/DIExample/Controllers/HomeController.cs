88a65af04f6b6641a3bf9bcd3be20ec52cb35120
Hassan Koraim <hassankoraim2@gmail.com>
2024-06-28 20:49:03 +0200

    first commit

---
+++ HarshaCourse/DIExample/DIExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+//using Services;
+using ServiceContracts;
+namespace DIExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private ICitiesService _citiesService;
+
+        //Constructor
+        public HomeController()
+        {
+            // Create object of citiesService class
+            // _citiesService = citiesService; //new CitiesService();
+        }
+        [Route("/")]
+        public IActionResult Index([FromServices] ICitiesService citiesService)
+        {
+            // _citiesService = citiesService;
+            List<string> cities = citiesService.getCities();
+            return View(cities);
+        }
+    }
+}
---
+++ HarshaCourse/DIExample/Services/CitiesService.cs
@@ -0,0 +1,17 @@
+﻿using ServiceContracts;
+namespace Services
+{
+    public class CitiesService : ICitiesService
+    {
+        private List<string> _cities;
+        public CitiesService()
+        {
+            _cities = new List<string>() { "New York", "London", "Paris" };
+        }   
+
+        public List<string> getCities()
+        {
+            return _cities;
+        }
+    }
+}
