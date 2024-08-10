5312db85a1f43b3e842394e4ed90d04c628685e4
Hassan Koraim <hassankoraim2@gmail.com>
2024-08-10 19:23:15 +0200

    Add EnvironmentExample Project in Harsh Environment Section

---
+++ HarshaCourse/EnvironmentExample/EnvironmentExample/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnvironmentExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewBag.CurrentEnvironment = _webHostEnvironment.EnvironmentName;
+            return View();
+        }
+        
+    }
+}
