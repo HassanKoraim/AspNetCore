3f2d6f7d1999beb1630adacad8109ab5d7aab3b6
Hassan Koraim <hassankoraim2@gmail.com>
2025-01-27 00:54:45 +0200

    Add 'CountriesControler' and two actions method for UploadCountriesFromExcel

---
+++ HarshaCourse/CRUDSolution/CRUDExample/Controllers/CountriesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceConstracts;
+
+namespace CRUDExample.Controllers
+{
+    [Route("[Controller]")]
+    public class CountriesController : Controller
+    {
+        private readonly ICountriesService _countiesService;
+        public CountriesController(ICountriesService countriesService)
+        {
+            _countiesService = countriesService;
+        }
+            [Route("[action]")]
+        [HttpGet]
+        public IActionResult UploadFromExcel()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<IActionResult> UploadFromExcel(IFormFile excelFile)
+        {
+            if (excelFile == null || excelFile.Length == 0)
+            {
+                ViewBag.ErrorMessage = "Please select an xlsx file";
+            }
+            if (!Path.GetExtension(excelFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+            {
+                ViewBag.ErrorMessage = "Unsupported file. 'xlsx' is expected";
+                return View();
+            }
+            int NCountriesInserted = await _countiesService.UploadCountriesFromExcelFile(excelFile);
+            ViewBag.Message = $"{NCountriesInserted} Countries Uploaded";
+            return View();
+        }
+    }
+}
