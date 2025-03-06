f62a4b1b21e4e628ac7fb2b070652894f1da99dd
Hassan Koraim <hassankoraim2@gmail.com>
2025-03-06 02:14:45 +0200

    Add RepositoryContracts

---
+++ HarshaCourse/CRUDSolution/RepositoryContracts/ICountriesRepository.cs
@@ -0,0 +1,38 @@
+﻿using Entities;
+
+namespace RepositoryContracts
+{
+    /// <summary>
+    /// Represents data access logic for managing Person Entity
+    /// </summary>
+    public interface ICountriesRepository
+    {
+        /// <summary>
+        /// Adds a new Country object to the data store
+        /// </summary>
+        /// <param name="country">Country object to add</param>
+        /// <returns>Returns the Country object after adding it to the data store</returns>
+        Task<Country> AddCountry(Country country);
+
+        /// <summary>
+        /// Returns all countries in the data store
+        /// </summary>
+        /// <returns>All countries from the table</returns>
+        Task<List<Country>> GetAllCountries();
+
+        /// <summary>
+        /// Returns a country object based on the given country id;
+        /// otherwise, it returns null
+        /// </summary>
+        /// <param name="CountryId">CountryId to search</param>
+        /// <returns>Matching Country or null</returns>
+        Task<char> GetCountryByCountryId(Guid CountryId);
+
+        /// <summary>
+        /// Returns a country object based on the given Country name
+        /// </summary>
+        /// <param name="CountryName">CountryName to search</param>
+        /// <returns>Matching Country or null</returns>
+        Task<Country> GetCountryByCountryName(string CountryName);
+    }
+}
---
+++ HarshaCourse/CRUDSolution/RepositoryContracts/IPersonsRepository.cs
@@ -0,0 +1,56 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+
+namespace RepositoryContracts
+{
+    /// <summary>
+    /// Repersents data access logic for managing Person Entity
+    /// </summary>
+    public interface IPersonsRepository
+    {
+        /// <summary>
+        /// Add a new Person object to the data store
+        /// </summary>
+        /// <param name="person">person to add</param>
+        /// <returns>Returns a Person object after adding it to the data store</returns>
+        Task<Person> AddPerson(Person person);
+
+        /// <summary>
+        /// Returns all Persons in the data store
+        /// </summary>
+        /// <returns>List of Person object from table</returns>
+        Task<List<Person>> GetAllPersons();
+
+        /// <summary>
+        /// Returns a Person object based on the given Person Id
+        /// </summary>
+        /// <param name="PersonId">PersonId to search</param>
+        /// <returns>Matching Person or null</returns>
+        Task<Person> GetPersonByPersonId(Guid PersonId);
+
+        /// <summary>
+        /// Returns all Person objects based on the given expression
+        /// </summary>
+        /// <param name="predicate">LINQ Expression to check</param>
+        /// <returns>All Matching Persons with given condition </returns>
+        Task<List<Person>> GetFilteredPersons(Expression<Func<Person, bool>> predicate);
+
+        /// <summary>
+        /// Delete Person object form data store
+        /// </summary>
+        /// <param name="PersonId">PersonId to search</param>
+        /// <returns>Returns True, if Deletion is successfull;
+        /// Otherwise, false</returns>
+        Task<bool> DeletePersonByPersonId(Guid PersonId);
+
+        /// <summary>
+        /// Update Person object based on given Person Id
+        /// </summary>
+        /// <param name="person">Person object to Update</param>
+        /// <returns>Returns a the Updated Person object </returns>
+        Task<Person> UpdatePerson(Person person);
+    }
+}
